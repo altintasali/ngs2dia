90ec35299c29cd04d38a9a0c2700bb62814c6270
Ali Altintas <altintas.ali@gmail.com>
2022-11-10 10:45:52 +0100

    gzcat >> zcat

--- bin/illumina_indexExtractor.sh
+++ bin/illumina_indexExtractor.sh
@@ -2,4 +2,4 @@
 FASTQ=$1
 OUTDIR=$2
 INDOUT=$(basename $FASTQ .gz).index
-gzcat $FASTQ | grep '^@' | cut -d : -f 10 | sort | uniq -c | sort -nr > $OUTDIR/$INDOUT
+zcat $FASTQ | grep '^@' | cut -d : -f 10 | sort | uniq -c | sort -nr > $OUTDIR/$INDOUT
